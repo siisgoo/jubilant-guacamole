5627a40ac3e702514dda8da74810c29d75547461
siisgoo <siisgoo@mail.ru>
2022-03-07 19:16:30 +0300

    modified:   .gitignore 	deleted:    log 	modified:   src/farm/towers.ts

--- src/farm/towers.ts
+++ src/farm/towers.ts
@@ -40,7 +40,7 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
     // ref
     private bot: HeroBot;
 
-    private sel: Set<puppeteer.ElementHandle<Element>>;
+    private ctx: Set<puppeteer.ElementHandle<Element>>;
 
     private locations:    Map<TowerLocs_t, puppeteer.ElementHandle>;
     private fightButtons: Map<string, puppeteer.ElementHandle>;
@@ -68,7 +68,7 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
 
             await this.scrapContext();
 
-            if (this.sel.size <= 0) {
+            if (this.ctx.size <= 0) {
                 throw new Error("No nawigation buttions");
             }
 
@@ -108,7 +108,7 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
             {
                 await this.scrapNearLocations();
 
-                logMessage("Going from capital...");
+                logMessage("Going from capital...\n");
                 await this.goToLocation(this.heroPreferedLocation());
                 return;
             }
@@ -131,7 +131,7 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
             }
             this.bot.Page.waitForNetworkIdle({timeout: 10000});
 
-            this.sel.clear();
+            this.ctx.clear();
             logMessage("End fight step" + '\n')
 
         } catch (err) {
@@ -162,14 +162,14 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
 
     private async scrapContext(): Promise<void> {
         logMessage("Scrumming context");
-        this.sel = new Set(await this.bot.Page.$$('div > a.flhdr'));
+        this.ctx = new Set(await this.bot.Page.$$('div > a.flhdr'));
     }
 
     private async scrapNearLocations(): Promise<void> {
         this.locations.clear();
 
         logMessage("Scrumming nearby locations")
-        for await (let el of this.sel) {
+        for await (let el of this.ctx) {
             let text: string = await el.getProperty('innerText').then(prop => prop.jsonValue());
             if (isTowerLoc(text)) {
                 this.locations.set(text, el);
@@ -194,7 +194,7 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
         this.fightButtons.clear();
 
         logMessage("Scrum fight buttons")
-        for await (let el of this.sel) {
+        for await (let el of this.ctx) {
             let text: string = await el.getProperty('innerText').then((el) => el.jsonValue());
             if ("Бить врагов" === text) {
                 this.fightButtons.set('Hit', el);
@@ -218,7 +218,7 @@ export class FarmTowers extends EventEmitter implements FarmStrategy {
         let button;
 
         logMessage("Trying use bottle")
-        for await (let el of this.sel) {
+        for await (let el of this.ctx) {
             let text: string = await el.getProperty('innerText') .then((el) => el.jsonValue());
             if (/Пить бутылочку */.test(text)) {
                 let inner = await el.$eval('span', e => e.textContent);
